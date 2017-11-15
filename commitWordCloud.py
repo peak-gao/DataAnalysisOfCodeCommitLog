d2c60ebef7a404f5934f793a7b18af44b0ea1068
feng gao <feng.gao.teleopti@outlook.com>
2017-11-15 14:52:05 +0800

    add resources

--- commitWordCloud.py
+++ commitWordCloud.py
@@ -14,7 +14,7 @@ def grey_color_func(word, font_size, position, orientation, random_state=None,
 
 d = path.dirname(__file__)
 
-mask = np.array(Image.open(path.join(d, "stormtrooper_mask.png")))
+mask = np.array(Image.open(path.join(d, "resources/stormtrooper_mask.png")))
 
 text =  ld.loadSummary()
 
@@ -24,7 +24,7 @@ wc = WordCloud(max_words=100, mask=mask, stopwords=stopwords, margin=10,
                random_state=1).generate(text)
 
 default_colors = wc.to_array()
-wc.to_file("a_new_hope.png")
+wc.to_file("commitWordCloud.png")
 plt.figure()
 plt.title("Commit Word Cloud")
 plt.imshow(default_colors, interpolation="bilinear")
